f33818636f5eed7b151c6c942ac6a44d337f0988
walksanatora <walkerffo22@gmail.com>
2025-07-28 10:13:10 -0700

    now it is serializer safe.

--- Content.Server/_Starlight/Paper/Actions/ActionRemoveComponent.cs
+++ Content.Server/_Starlight/Paper/Actions/ActionRemoveComponent.cs
@@ -1,3 +1,5 @@
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Generic;
 using Robust.Shared.Utility;
 
 namespace Content.Server._Starlight.Paper.Actions;
@@ -7,27 +9,20 @@ public sealed partial class ActionRemoveComponent : OnSignAction
     /// <summary>
     /// list of component names to be deleted.
     /// </summary>
-    [DataField]
-    public List<String> Components = [];
-    
+    [DataField(customTypeSerializer: typeof(CustomHashSetSerializer<string, ComponentNameSerializer>))]
+    public HashSet<string> Components = [];
+
     private IComponentFactory _componentFactory = default!;
     private IEntityManager _entityManager = default!;
-    
+
     public override bool Action(EntityUid paper, ActionsOnSignComponent component, EntityUid target)
     {
         foreach (var rmComp in Components)
         {
-            var targetComp = _componentFactory.GetComponent(rmComp);
-            var remComp = _entityManager.GetType().GetMethod(nameof(IEntityManager.RemoveComponent));
-            if (remComp == null)
-            {
-                DebugTools.Assert("Failed to reflect RemoveComponent from IEntityManager instance");
-                continue; // Failed to reflect... this is a MAJOR bug   
-            }
-            var generic = remComp.MakeGenericMethod(targetComp.GetType());
-            generic.Invoke(_entityManager, [target]);
+            if (_componentFactory.TryGetRegistration(rmComp, out var registration))
+                _entityManager.RemoveComponent(target, registration.Type);
         }
-        
+
         return false;
     }
 
