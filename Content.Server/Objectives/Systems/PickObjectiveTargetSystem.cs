b802b3079193a22d4708ed4a5935e2399c4585e5
neomoth <69372252+neomoth@users.noreply.github.com>
2025-10-29 15:57:39 -0400

    add NoObjectiveTargetComponent
    also made edits to ParadoxCloneRuleSystem and PickObjectiveTargetSystem, as well as adding to alerts.ftl

--- Content.Server/Objectives/Systems/PickObjectiveTargetSystem.cs
+++ Content.Server/Objectives/Systems/PickObjectiveTargetSystem.cs
@@ -5,6 +5,7 @@ using Content.Server.GameTicking.Rules;
 using Content.Server.Revolutionary.Components;
 using Robust.Shared.Random;
 using System.Linq;
+using Content.Shared.Mind.Filters;
 
 namespace Content.Server.Objectives.Systems;
 
@@ -23,7 +24,17 @@ public sealed class PickObjectiveTargetSystem : EntitySystem
 
         SubscribeLocalEvent<PickSpecificPersonComponent, ObjectiveAssignedEvent>(OnSpecificPersonAssigned);
         SubscribeLocalEvent<PickRandomPersonComponent, ObjectiveAssignedEvent>(OnRandomPersonAssigned);
+        SubscribeLocalEvent<PickRandomPersonComponent, ComponentStartup>(OnComponentStartup); // SL add
     }
+
+    // SL start
+    private void OnComponentStartup(Entity<PickRandomPersonComponent> ent, ref ComponentStartup args)
+    {
+        // inject new filter blacklisting NoObjectiveTargetComponent
+        var filter = new BodyMindFilter { Whitelist = { Components = ["NoObjectiveTarget"] }, Inverted = true };
+        ent.Comp.Filters.Add(filter);
+    }
+    // SL end
 
     private void OnSpecificPersonAssigned(Entity<PickSpecificPersonComponent> ent, ref ObjectiveAssignedEvent args)
     {
